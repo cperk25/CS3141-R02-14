88d8d4cf2832d604d16425f48adacc80cd305159
jjradfor <31937062+Heskitgel@users.noreply.github.com>
2022-10-14 12:48:44 -0400

    revising index and sketch, compiled initial prototype
    creating interface using electron wrapper Nativefier to convert our website into an executable.
    --Note: the .exe executable of our program is too large to store on github (100+MB), meaning in order to test nativefier we will need to compile it ourselves. however we can test the program just fine in browser during development

--- project/sketch.js
+++ project/sketch.js
@@ -1,14 +1,14 @@
 function setup() {
   // put setup code here
-  createCanvas(400, 400);
+  createCanvas(1280, 720);
 }
 
 function draw() {
   // put drawing code here
    if (mouseIsPressed) {
     fill(0);
+    ellipse(mouseX, mouseY, 80, 80);
   } else {
     fill(255);
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
