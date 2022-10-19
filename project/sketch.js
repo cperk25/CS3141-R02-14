3f091deb3e9ae965ec294de60f62da2bb7d895ff
Ellie Sackrider <elsackrider612@gmail.com>
2022-10-19 01:21:50 -0400

    Updated home page UI

--- project/sketch.js
+++ project/sketch.js
@@ -5,8 +5,6 @@
 let CANVAS_WIDTH = 1280;
 let CANVAS_HEIGHT = 720;
 let FR = 200; // Framerate of our app, the higher the smoother our drawing
-let gameButton;
-let practiceButton;
 let gameStart = false; // determines if home menu buttons have been pressed
 let practiceStart = false; // determines if practice button was pressed
 
@@ -32,21 +30,47 @@ function setup(){
 	// draws background of hex code #ADD8E6
 	background(173, 216, 230);
 	
-	// home screen buttons
+	/* home screen buttons */
 	// play drawing game
 	gameButton = new Clickable(); // create button
-	gameButton.locate(CANVAS_WIDTH/4, CANVAS_HEIGHT/2); // position button
+	gameButton.resize(150, 75);
+	gameButton.locate(CANVAS_WIDTH/4, (CANVAS_HEIGHT*2)/3); // position button
 	gameButton.text = "Play Game"; // text on the button
+	gameButton.textSize = 20;
+	
+	// set normal button color
+	gameButton.onOutside = function(){
+		gameButton.color = "#DBDBDB";
+	}
+	
+	// change button color when hovered over
+	gameButton.onHover = function(){
+		gameButton.color = "#B5B5B5";
+	}
+	
 	// when button is pressed, hide it and bring up the drawing canvas
 	gameButton.onRelease = function(){
 		gameStart = true;
 		background(173, 216, 230);
 	}
 	
-	// go to practice mode
+	// practice drawing mode
 	practiceButton = new Clickable(); // create button
-	practiceButton.locate((CANVAS_WIDTH*2)/3, CANVAS_HEIGHT/2); // position button
+	practiceButton.resize(150, 75);
+	practiceButton.locate(CANVAS_WIDTH*0.60, (CANVAS_HEIGHT*2)/3); // position button
 	practiceButton.text = "Practice"; // text on the button
+	practiceButton.textSize = 20;
+	
+	// set normal button color
+	practiceButton.onOutside = function(){
+		practiceButton.color = "#DBDBDB";
+	}
+	
+	// change button color when hovered over
+	practiceButton.onHover = function(){
+		practiceButton.color = "#B5B5B5";
+	}
+	
 	// when button is pressed, hide it and bring up the drawing canvas
 	practiceButton.onRelease = function(){
 		practiceStart = true;
@@ -61,6 +85,12 @@ function draw() {
 		// add buttons to the canvas
 		gameButton.draw();
 		practiceButton.draw();
+		
+		// add text to home page displaying the name of the game
+		textSize(100);
+		textFont("helvetica");
+		textAlign(CENTER);
+		text('Speed Draw ✏️', CANVAS_WIDTH/2, CANVAS_HEIGHT/3);
 	}
 	
 	// draw() is called every frame, think of it as our main() method
@@ -71,14 +101,14 @@ function draw() {
 		if (gameStart === true)
 		{
 			image(ui_timer, 0, 0, (CANVAS_WIDTH/10), (CANVAS_HEIGHT/8));
-			image(ui_toolbar, (CANVAS_WIDTH/4), 0, (CANVAS_WIDTH/5), (CANVAS_HEIGHT/8));
 		}
 
 		// displays our temporary UI art for our color selection bar
 		// first is the image variable, then the x position, y position,
 		// and then the width and height
 		image(ui_colorbar, (CANVAS_WIDTH/2), 0, (CANVAS_WIDTH/2), (CANVAS_HEIGHT/8));
-
+		image(ui_toolbar, (CANVAS_WIDTH/4), 0, (CANVAS_WIDTH/5), (CANVAS_HEIGHT/8));
+		
 		// places our color UI halfway across the X and 1/8th across the Y
 		
 		// sample drawing code
