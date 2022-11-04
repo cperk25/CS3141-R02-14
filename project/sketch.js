08d4b5f9a6e8ea6969283ed7ae609ce9e3d952f2
jjradfor <31937062+Heskitgel@users.noreply.github.com>
2022-11-04 08:53:49 -0400

    Added rudimentary opacity functionality
    added opacity slider buttons and functionality by seperating the colors of the current color and adding them back together with opacity included

--- project/sketch.js
+++ project/sketch.js
@@ -10,6 +10,7 @@ let CANVAS_HEIGHT = 720;
 let FR = 200; // Framerate of our app, the higher the smoother our drawing
 let gameStart = false; // determines if home menu buttons have been pressed
 let practiceStart = false; // determines if practice button was pressed
+let currentOpacity = 100;
 
 // Tracks current brush color; initialized to black
 var currentColor = "black";
@@ -81,6 +82,55 @@ function setup(){
 		background(173, 216, 230);
 	}
 	
+	opacityIncrease = new Clickable();
+	opacityIncrease.resize(50, 40);
+	opacityIncrease.locate(((CANVAS_WIDTH/5) + 200), 10);
+	opacityIncrease.text = "↑";
+	opacityIncrease.textSize = 20;
+	opacityIncrease.onPress = function() {
+		if (currentOpacity < 100){
+			currentOpacity+=5;
+			displayOpacity.text = currentOpacity + "%";
+			currentColor = setOpacity(currentColor, currentOpacity);
+		}
+	}
+
+	opacityIncrease.onOutside = function() {
+		opacityIncrease.color = "#DBDBDB";
+	}
+
+	opacityIncrease.onHover = function(){
+		opacityIncrease.color = "#B5B5B5";
+	}
+
+	displayOpacity = new Clickable();
+	displayOpacity.resize(100, 100);
+	displayOpacity.locate((CANVAS_WIDTH/5) + 100, 0);
+	displayOpacity.textSize = 30;
+	displayOpacity.text = currentOpacity + "%";
+
+	opacityDecrease = new Clickable();
+	opacityDecrease.resize(50, 40);
+	opacityDecrease.locate(((CANVAS_WIDTH/5) + 200), 50);
+	opacityDecrease.text = "↓";
+	opacityDecrease.textSize = 20;
+	opacityDecrease.onPress = function(){
+		if (currentOpacity > 5)
+		{
+			currentOpacity-=5;
+			displayOpacity.text = currentOpacity + "%";
+			currentColor = setOpacity(currentColor, currentOpacity);
+		}
+	}
+	// set normal button color
+	opacityDecrease.onOutside = function(){
+		opacityDecrease.color = "#DBDBDB";
+	}
+	// change color when hovered over
+	opacityDecrease.onHover = function(){
+		opacityDecrease.color = "#B5B5B5";
+	}
+
 	// increase line size by pressing button
 	sizeIncrease = new Clickable();
 	sizeIncrease.resize(50, 40);
@@ -140,6 +190,7 @@ function setup(){
 	whiteButton.color = "white";
 	whiteButton.onPress = function(){
 		currentColor = "white"; //change brush color to white
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 
 	grayButton = new Clickable(); //gray
@@ -149,6 +200,7 @@ function setup(){
 	grayButton.color = "gray";
 	grayButton.onPress = function(){
 		currentColor = "gray"; //change brush color to gary
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	blackButton = new Clickable(); //black
@@ -158,6 +210,7 @@ function setup(){
 	blackButton.color = "black";
 	blackButton.onPress = function(){
 		currentColor = "black"; //change brush color to black
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	maroonButton = new Clickable(); //maroon
@@ -167,6 +220,7 @@ function setup(){
 	maroonButton.color = "maroon";
 	maroonButton.onPress = function(){
 		currentColor = "maroon"; //change brush color to maroon
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	redButton = new Clickable(); //red
@@ -176,6 +230,7 @@ function setup(){
 	redButton.color = "red";
 	redButton.onPress = function(){
 		currentColor = "red"; //change brush color to red
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	orangeButton = new Clickable(); //orange
@@ -185,6 +240,7 @@ function setup(){
 	orangeButton.color = "orange";
 	orangeButton.onPress = function(){
 		currentColor = "orange"; //change brush color to orange
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	yellowButton = new Clickable(); //yellow
@@ -194,6 +250,7 @@ function setup(){
 	yellowButton.color = "yellow";
 	yellowButton.onPress = function(){
 		currentColor = "yellow"; //change brush color to yellow
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	limeButton = new Clickable(); //light green or lime
@@ -203,6 +260,7 @@ function setup(){
 	limeButton.color = "lime";
 	limeButton.onPress = function(){
 		currentColor = "lime"; //change brush color to lime
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	greenButton = new Clickable(); //green
@@ -212,6 +270,7 @@ function setup(){
 	greenButton.color = "green";
 	greenButton.onPress = function(){
 		currentColor = "green"; //change brush color to green
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	cyanButton = new Clickable(); //cyan
@@ -221,6 +280,7 @@ function setup(){
 	cyanButton.color = "cyan";
 	cyanButton.onPress = function(){
 		currentColor = "cyan"; //change brush color to cyan
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	blueButton = new Clickable(); //blue
@@ -230,6 +290,7 @@ function setup(){
 	blueButton.color = "blue";
 	blueButton.onPress = function(){
 		currentColor = "blue"; //change brush color to blue
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	navyButton = new Clickable(); //navy
@@ -239,6 +300,7 @@ function setup(){
 	navyButton.color = "navy";
 	navyButton.onPress = function(){
 		currentColor = "navy"; //change brush color to navy
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	purpleButton = new Clickable(); //purple
@@ -248,6 +310,8 @@ function setup(){
 	purpleButton.color = "purple";
 	purpleButton.onPress = function(){
 		currentColor = "purple"; //change brush color to purple
+		currentColor = setOpacity(currentColor, currentOpacity);
+
 	}
 	
 	lightpinkButton = new Clickable(); //light pink
@@ -257,6 +321,7 @@ function setup(){
 	lightpinkButton.color = "lightpink";
 	lightpinkButton.onPress = function(){
 		currentColor = "lightpink"; //change brush color to light pink
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	fuchsiaButton = new Clickable(); //fuschsia or dark pink
@@ -266,6 +331,7 @@ function setup(){
 	fuchsiaButton.color = "fuchsia";
 	fuchsiaButton.onPress = function(){
 		currentColor = "fuchsia"; //change brush color to fuchsia
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 	
 	brownButton = new Clickable(); //brown
@@ -275,11 +341,12 @@ function setup(){
 	brownButton.color = "brown";
 	brownButton.onPress = function(){
 		currentColor = "brown"; //change brush color to brown
+		currentColor = setOpacity(currentColor, currentOpacity);
 	}
 
 	backButton = new Clickable(); // back button to return back to main menu
 	backButton.resize(CANVAS_WIDTH/10, CANVAS_HEIGHT/8);
-	backButton.locate(((CANVAS_WIDTH/10)), 0) // position it next to our temporary timer with a padding of 10px
+	backButton.locate(((CANVAS_WIDTH/10)), 0) // position it next to our temporary timer 
 	backButton.text = "Back";
 	backButton.textSize = 20;
 
@@ -344,6 +411,11 @@ function draw() {
 		sizeDecrease.draw();
 		displaySize.draw();
 
+		// add in opacity buttons
+		displayOpacity.draw();
+		opacityIncrease.draw();
+		opacityDecrease.draw();
+
 		// add first row of color buttons
 		whiteButton.draw();
 		grayButton.draw();
@@ -363,4 +435,15 @@ function draw() {
 		fuchsiaButton.draw();
 		brownButton.draw();
 	}
+}
+
+function setOpacity(inputColor, opacityPercent){
+	console.log("setting " + opacityPercent + " opacity for " + inputColor);
+	let r = red(inputColor);
+	let g = green(inputColor);
+	let b = blue(inputColor);
+	let a = (opacityPercent * 2.55);
+	console.log("a = " + a);
+	let returnColor = color(r, g, b, a);
+	return returnColor;
 }
